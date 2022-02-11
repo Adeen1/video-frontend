14b35bd352a8ccec13b9a8f1f114b808374906a5
Ahmad <adeenahmad56@gmail.com>
2022-02-11 15:45:39 +0500

    done

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import { Typography, AppBar } from "@material-ui/core";
 import VideoPlayer from "./components/VideoPlayer";
 import Option from "./components/Option";
 import Notifications from "./components/Notification";
+import { makeStyles } from "@material-ui/core/styles";
+
 const useStyles = makeStyles((theme) => ({
   appBar: {
     borderRadius: 15,
